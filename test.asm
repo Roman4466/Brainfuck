33754e266ccbece834e303473dc1cc5400b69709
Roman <roman.malynovsky@gmail.com>
2024-02-28 07:33:13 +0200

    Reads input, wait till ^Z symbol and output something

--- test.asm
+++ test.asm
@@ -1,42 +1,43 @@
 .model small
 .stack 100h
 .data
-    char    db '?$'            ; Placeholder for input character
-    memory  dw 10000 dup(0)    ; Brainfuck memory tape
-    program dw 10000 dup(0)    ; Placeholder for Brainfuck program code
+    char    db '?'             ; Placeholder for input character
+    ; memory  db 10000 dup(0)    ; Brainfuck memory tape should be bytes
+    program db 10000 dup(0)    ; Placeholder for Brainfuck program code
+    count   dw 0               ; Counter for the number of characters read
 
 .code
     start:          
                     mov ax, @data
                     mov ds, ax
-                    xor bx, bx
-                    xor cx, cx                ; Set loop counter for columns (Y)
+                    xor bx, bx                ; Offset index for the program data
 
-    read_charachter:
+    read_character: 
                     mov ah, 01h               ; DOS function: Read character from standard input
-                    int 21h
-    ; mov char, al ; Store the read character
-                    xor ah, ah
-                    mov [program + bx], ax
-                    inc bx
-    
-                    inc cx
-                    cmp cx, 4
-                    jne read_charachter       ; Repeat if cx != 16
-
-                    xor bx, bx                ; Reset offset index for output
-    ; Now, let's print the character back to the console
-    print_character:
-                    cmp bx, 4                ; Check if we have printed 16 characters
-                    je  done                  ; If so, we are done
-
-                    xor dh, dh
-                    mov dx, [program + bx]    ; Load the next character to print
-                    mov ah, 02h               ; DOS function: Print character in DL
                     int 21h                   ; Call DOS interrupt
+                    cmp al, 1Ah               ; Check if input is Ctrl+Z (EOF)
+                    je  done_reading          ; If it is EOF, jump to done_reading
+                    mov [program + bx], al    ; Store the read character
+                    inc bx                    ; Move to the next position in the program array
+                    inc [count]               ; Increment the character count
+                    cmp bx, 10000             ; Check if we've reached the end of the program buffer
+                    jb  read_character        ; If not, read next character
 
-                    inc bx                    ; Move to the next character
-                    jmp print_character       ; Continue printing characters
+    done_reading:   
+                    xor bx, bx                ; Reset offset index for output
+
+    print_character:                          ; Move to the next position in the program array
+                    inc bx                    ; Increment the character count
+                    cmp bx, [count]           ; Check if we've reached the end of the program buffer
+                    ; jb  done                  ; If so, we are done
+
+                    mov dx, [count]
+                    xor dh, dh                ; Clear the high byte of DX
+                    mov al, dl    ; Load the next character to print
+                    mov ah, 02h               ; DOS function: Print character in DL
+                    int 21h                   ; Move to the next character
+                    ; jmp print_character       ; Continue printing characters
+
 
     done:           
                     mov ax, 4C00h             ; Terminate the program
