c74789b82cae5a72476865736d5a26a12bc837e1
Roman <roman.malynovsky@gmail.com>
2024-03-01 18:15:10 +0200

    Changed memory size from db to dw

--- entry.asm
+++ entry.asm
@@ -1,9 +1,9 @@
 .model small
 .stack 100h
 .data
-    memory  db 10000 dup(0)    ; Brainfuck memory tape should be bytes
+    memory  dw 10000 dup(0)    ; Brainfuck memory tape should be bytes
     program db 10000 dup(0)    ; Placeholder for Brainfuck program code
-    count   db 0               ; Counter for the number of characters read
+    count   dw 0               ; Counter for the number of characters read
 
 .code
     start:            
@@ -16,17 +16,17 @@
                       cmp  al, 1Ah               ; Check if input is Ctrl+Z (EOF)
                       je   done_reading
                       mov  [program + bx], al
-                      inc  bl                    ; Move to the next position in the program array
+                      inc  bx                    ; Move to the next position in the program array
                       cmp  bx, 10000             ; Check if we've reached the end of the program buffer
                       jb   read_character        ; If not, read next character
 
     done_reading:     
-                      mov  [count], bl
+                      mov  [count], bx
                       xor  cx, cx
                       xor  bx, bx
     main_program_loop:
     ; If so, we are done
-                      cmp  cl, [count]
+                      cmp  cx, [count]
                       jae  exit                  ; If CX is greater or equal to [count], we are done
 
     ; Fetch the current command from the program
@@ -36,8 +36,8 @@
                       pop  bx
                       inc  cl                    ; Increment the program counter
 
-                    ;   mov  ah, 02h               ; DOS function: Print character in DL
-                    ;   int  21h
+    ;   mov  ah, 02h               ; DOS function: Print character in DL
+    ;   int  21h
     ; Compare the command and jump to the corresponding routine
 
                       cmp  dl, '+'
@@ -52,33 +52,34 @@
                       je   shift_right
                       cmp  dl, '<'
                       je   shift_left
-    ;  cmp  dl, '['
-    ;  je   begin_loop
-    ;  cmp  dl, ']'
-    ;  je   end_loop
+                      cmp  dl, '['
+                      je   begin_loop
+                      cmp  dl, ']'
+                      je   end_loop
                       jmp  main_program_loop
 
     increment:        
-                      mov  al, [memory + bx]
-                      inc  al
-                      mov  [memory + bx], al
+                      mov  ax, [memory + bx]
+                      inc  ax
+                      mov  [memory + bx], ax
                       jmp  main_program_loop
 
     decrement:        
-                      mov  al, [memory + bx]
-                      dec  al
-                      mov  [memory + bx], al
+                      mov  ax, [memory + bx]
+                      dec  ax
+                      mov  [memory + bx], ax
                       jmp  main_program_loop
 
 
     input:            
                       mov  ah, 01h               ; DOS function: Read character from standard input
                       int  21h                   ; Call DOS interrupt
-                      mov  [memory + bx], al
+                    ;   xor ah, ah
+                      mov  [memory + bx], ax
                       jmp  main_program_loop
 
     output:           
-                      mov  dl, [memory + bx]     ; Load the next character to print
+                      mov  dx, [memory + bx]     ; Load the next character to print
                       mov  ah, 02h               ; DOS function: Print character in DL
                       int  21h                   ; Call DOS interrupt
                       jmp  main_program_loop
@@ -89,6 +90,16 @@
 
     shift_right:      
                       inc  bx
+                      jmp  main_program_loop
+
+    begin_loop:       
+                      push cx
+                      jmp  main_program_loop
+    
+    end_loop:         
+                      cmp  [memory + bx], 0      ; Check if the current cell is zero
+                      jne  main_program_loop
+                      pop  cx
                       jmp  main_program_loop
 
     exit:             
