adc05359c1ed335cda2ebea5ceb48c1cde7da5cf
Roman <r.malynovskyi@ukma.edu.ua>
2024-03-02 21:12:53 +0200

    Rewtote code so it will start test

--- entry.asm
+++ entry.asm
@@ -3,9 +3,34 @@
          org 0100h    ; Counter for the number of characters read
 
 .code
-    start:            
-                      mov  ax, @data
-                      mov  ds, ax
+                      mov  ah, 0Ah               ; Buffered input function
+                      lea  dx, buffer            ; DS:DX points to the input buffer
+                      int  21h                   ; Call DOS interrupt
+
+    ; The input buffer will contain the length of the input and the input itself
+    ; Parse the buffer to extract the filename (not implemented here)
+    
+    ; Assuming the filename is now in 'buffer' and is null-terminated
+    ; Open the Brainfuck code file
+                      mov  ah, 3Dh               ; Open file function
+                      lea  dx, buffer            ; DS:DX points to the filename in the buffer
+                      mov  al, 0                 ; Open file for reading (access mode)
+                      int  21h                   ; Call DOS interrupt
+                      jc   open_error            ; Jump to error handling if carry flag is set
+                      mov  bx, ax                ; File handle is returned in AX
+
+    ; Read the Brainfuck code into the 'program' buffer
+                      mov  ah, 3Fh               ; Read file or device function
+                      lea  dx, program           ; DS:DX points to the program buffer
+                      mov  cx, 10000             ; Number of bytes to read
+                      int  21h                   ; Call DOS interrupt
+                      jc   read_error            ; Jump to error handling if carry flag is set
+                      mov  [count], ax           ; Number of bytes read returned in AX
+
+    ; Close the file
+                      mov  ah, 3Eh               ; Close file function
+                      int  21h                   ; Call DOS interrupt
+                      jc   close_error           ; Jump to error handling if carry flag is set
 
     read_character:   
                       mov  ah, 01h               ; DOS function: Read character from standard input
@@ -103,7 +128,10 @@
                       mov  ax, 4C00h             ; Terminate the program
                       int  21h
 
-                      
+    buffer            db   255                   ; Maximum command line length
+                      db   ?                     ; Actual length read
+                      db   100 dup(0)            ; Buffer to store the command line
+
     memory            dw   10000 dup(0)          ; Brainfuck memory tape should be bytes
     program           db   10000 dup(0)          ; Placeholder for Brainfuck program code
     count             dw   0
